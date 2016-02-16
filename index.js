3f275303bcf99e77277d5768e3f19e0f1ef5972a
Dzmitry Frolov <dzmitryfrolov@gmail.com>
2016-02-16 15:44:35 +0700

    remove promisify

--- index.js
+++ index.js
@@ -1,112 +1,53 @@
 'use strict'
 
-var Promise = require('bluebird'),
-    mysql   = require('mysql2'),
-    Cache = require('nscp'),
-    cache = new Cache({ stdTTL: 3600, ttlPercent: 70 }),
-    hit = require('debug')('hq:cache:hit'),
-    miss = require('debug')('hq:cache:miss'),
-    debug = require('debug')('hq:mysql'),
-    log,
-    mysqlCluster
+const mysql = require('mysql2')
+const Promise = require('bluebird')
+const debug = require('debug')('hq:mysql')
+const error = require('error')('hq:mysql:error')
 
-Promise.promisifyAll(mysql)
-Promise.promisifyAll(require('mysql2/lib/pool_cluster').prototype)
+module.exports = function (params) {
+  const verify = []
+  const groups = params.conf.mysqlGroups
 
-module.exports = function(params) {
-    let groups = params.conf.mysqlGroups,
-        verify = []
+  const mysqlCluster = mysql.createPoolCluster()
+  mysqlCluster.clusterExecute = execute
 
-    mysqlCluster = mysql.createPoolCluster()
-    mysqlCluster.clearCache = clearCache
-    mysqlCluster.clusterExecute = execute
-    mysqlCluster.cachedClusterExecute = cachedExecute
-    mysqlCluster.makeKey = makeKey
-    mysqlCluster.firstResult = firstResult
-    Promise.promisifyAll(mysqlCluster)
-
-    for (let i = 0; i < groups.length; i++) {
-        mysqlCluster.add(groups[i].groupName, groups[i])
-        verify.push(mysqlCluster.clusterExecute('SELECT 1 + ? AS ANSWER',[i],groups[i].groupName)
-            .then(function(result){
-                debug('connection to',groups[i].host,'established',result)
-                return result
-            }))
-    }
-
-    function clearCache() {
-        cache.clear()
-    }
-
-    function makeKey(parts) {
-        let out
-
-        if ( Array.isArray(parts) ) {
-            out = parts
-        } else {
-            out = Array.prototype.slice.call(arguments)
+  function execute(query, params, group) {
+    group = group || '*'
+    return new Promise((resolve, reject) => {
+      mysqlCluster.getConnection(group, (err, connection) => {
+        if (err) {
+          return reject(err)
         }
-        return out.join(':')
-    }
 
-    function firstResult(value) {
-        return value[0] || value[0] === 0 ? value[0] : value
-    }
+        connection.config.namedPlaceholders = !Array.isArray(params)
+        connection.query(query, params, (err, rows) => {
+          connection.release()
 
-    function cachedExecute(key, query, params, group) {
-        let toReturn = cache.get(key),
-            ttl
-        if ( typeof query === 'object' && query.ttl ) {
-            ttl = query.ttl
-        }
-        if (toReturn) {
-            return Promise.resolve(toReturn)
-        } else {
-            return execute(query, params, group)
-                .bind({key: key, ttl: ttl})
-                .then(updateCache)
-        }
-    }
+          if (err) {
+            return reject(err)
+          }
 
-    function updateCache(result) {
-        this.ttl ? cache.set(this.key,result,this.ttl) : cache.set(this.key,result)
-        return result
-    }
-
-    function execute(query, params, group) {
-        return Promise.using(getConnection(group),onConnection)
-
-        function onConnection(connection) {
-             connection.config.namedPlaceholders = !Array.isArray(params)
-
-            return connection.executeAsync(query,params)
-                .spread(function(results,_) {
-                    if ( Array.isArray(results) && results.length ) {
-                        return results[0][0] && results[0][0].length < 1 ? results[0][0] : results[0]
-                    } else {
-                        return results
-                    }
-                })
-        }
-    }
-
-    function getConnection(group) {
-        group = group || '*'
-        return mysqlCluster.getConnectionAsync(group)
-            .disposer(function(connection) {
-                connection.release()
-            })
-    }
-
-    //verify atleast one connection
-    return Promise.all(verify)
-        .then(function(answers){
-            debug('complete round robin: ',answers)
-            return mysqlCluster
+          resolve(rows)
         })
-        .catch(function(err) {
-            console.log(err)
-            process.exit(1)
-        })
+      })
+    })
+  }
 
+  for (let i = 0; i < groups.length; i++) {
+    mysqlCluster.add(groups[i].groupName, groups[i])
+
+    verify.push(mysqlCluster.clusterExecute('SELECT 1 + ? AS ANSWER', [i], groups[i].groupName)
+      .then(result => debug('connection to', groups[i].host, 'established', result)))
+  }
+
+  return Promise.all(verify)
+    .then(function (answers) {
+      debug('complete round robin: ', answers)
+      return mysqlCluster
+    })
+    .catch(function (err) {
+      console.log(err)
+      process.exit(1)
+    })
 }
